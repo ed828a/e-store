6d2dd74cda66b935dbbc6253773918d840ee0fbf
ed828a <ed828a@gmail.com>
2020-01-28 09:52:47 +1030

    add evn variable

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -4,7 +4,7 @@ import CartColumns from './CartColumns'
 import EmptyCart from './EmptyCart'
 import { ProductConsumer } from '../../ProductProvider'
 import CartList from './CartList'
-import CartTotals from './CartTotals'
+import CartTotals from './CartTotals' 
 
 export default class Cart extends Component {
     render() {
@@ -19,7 +19,7 @@ export default class Cart extends Component {
                                     <Title name='your' title='cart' />
                                     <CartColumns />
                                     <CartList value={value}/>
-                                    <CartTotals value={value} />
+                                    <CartTotals value={value} history={this.props.history}/>
                                 </React.Fragment>
                             );
                         } else {
